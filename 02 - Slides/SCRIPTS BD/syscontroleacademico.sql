fb255a95a3a7696c5667cf55481675fd493a3d63
joyceMiranda <mds.joyce@gmail.com>
2017-08-08 15:16:50 -0300

    ...

--- 02 - Slides/SCRIPTS BD/syscontroleacademico.sql
+++ 02 - Slides/SCRIPTS BD/syscontroleacademico.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS "syscontroleacademico" /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE "syscontroleacademico";
 -- MySQL dump 10.13  Distrib 5.7.18, for Win64 (x86_64)
 --
 -- Host: localhost    Database: syscontroleacademico
@@ -18,28 +16,52 @@ USE "syscontroleacademico";
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table "curso"
+-- Table structure for table `aluno`
 --
 
-DROP TABLE IF EXISTS "curso";
+DROP TABLE IF EXISTS aluno;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE "curso" (
-  "idcurso" int(11) NOT NULL AUTO_INCREMENT,
-  "sigla" varchar(15) NOT NULL,
-  "descricao" varchar(200) NOT NULL,
-  PRIMARY KEY ("idcurso")
+CREATE TABLE aluno (
+  idaluno bigint(20) NOT NULL,
+  nome varchar(100) DEFAULT NULL,
+  idCurso bigint(20) DEFAULT NULL,
+  PRIMARY KEY (idaluno),
+  KEY idCurso_idx (idCurso),
+  CONSTRAINT idCurso FOREIGN KEY (idCurso) REFERENCES curso (idCurso) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table "curso"
+-- Table structure for table `curso`
 --
 
-LOCK TABLES "curso" WRITE;
-/*!40000 ALTER TABLE "curso" DISABLE KEYS */;
-/*!40000 ALTER TABLE "curso" ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS curso;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE curso (
+  idCurso bigint(20) NOT NULL AUTO_INCREMENT,
+  descricao varchar(255) DEFAULT NULL,
+  sigla varchar(255) DEFAULT NULL,
+  PRIMARY KEY (idCurso)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS usuario;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE usuario (
+  idUsuario bigint(20) NOT NULL AUTO_INCREMENT,
+  login varchar(50) DEFAULT NULL,
+  senha varchar(6) DEFAULT NULL,
+  PRIMARY KEY (idUsuario),
+  UNIQUE KEY login (login)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -50,4 +72,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-24 15:32:03
+-- Dump completed on 2017-08-08  1:03:33
